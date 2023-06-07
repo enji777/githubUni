cccadee709fccfa8626fbd4f5d3432c74ac8223f
enji777 <hnyeen12@gmail.com>
2023-06-07 22:34:57 +0200

    1. Version Pflichtabgabe 3

--- Intellij/Pflichtabgabe3/src/Dreieck.java
+++ Intellij/Pflichtabgabe3/src/Dreieck.java
@@ -3,29 +3,44 @@ public class Dreieck {
     private double seite;
 
 
-    public Dreieck(double seite){
-        this.seite = seite;
+    public Dreieck(double seite) {
+        if (seite > 0) {
+            this.seite = seite;
+        }
+        else {
+            this.seite = 0;
+        }
     }
 
-    // e)
-    public void skaliereDreieck() {
+    public void skaliereDreieck(double faktor) {
+    seite = seite * faktor;
+    }
+
+    public double getSeite() {
+        return seite;
+    }
+
+    public void setSeite(double seite) {
+        if (seite > 0) {
+            this.seite = seite;
+        }
+        else {
+            this.seite = 0;
+        }
+    }
+
+    public double berechneHoehe(){
+        return seite*(Math.sqrt(3)/2);
+    }
+
+    public double berechneFlaeche(){
+        return Math.pow(seite,2) * Math.sqrt(3)/4;
+    }
+
+    public boolean vergleicheDreieck(Dreieck d){
+       return (this.seite == d.seite);
 
     }
 
-    // f)
-    public void berechneHoehe() {
-
-    }
-
-    // g)
-
-    public void berechneFlaeche() {
-
-    }
-
-    // h)
-    public void vergleicheDreieck() {
-
-    }
-
+   // public double berechneFlaeche2();
 }
--- Intellij/Pflichtabgabe3/src/TestDreieck.java
+++ Intellij/Pflichtabgabe3/src/TestDreieck.java
@@ -3,6 +3,26 @@
 public class TestDreieck {
     public static void main(String[] args) {
 
+        Dreieck d1 = new Dreieck(0);
+
+        d1.setSeite(7);
+
+        Dreieck d2 = new Dreieck(3.2);
+
+
+        System.out.println(d1.berechneFlaeche());
+
+        d1.skaliereDreieck(1.5);
+
+        System.out.println(d1.getSeite());
+
+        System.out.println(d2.berechneHoehe());
+
+        System.out.println(d1.vergleicheDreieck(d2));
+
+
+    }
+
 
 
 
