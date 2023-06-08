3bfc75189a2f39db8d8a1cb3f38d7d33ff51986e
enji777 <enji777>
2023-06-08 14:58:17 +0200

    Arrays 8.6.2023

---
+++ Intellij/Arrays/src/MeinArray.java
@@ -0,0 +1,10 @@
+public class MeinArray {
+
+    public boolean istGleichlang(float[] a1, float[] a2){
+
+        if (a1.length == a2.length)
+            return true;
+        else {
+            return false;
+        }
+    }
---
+++ Intellij/Arrays/src/MeinTestArray.java
@@ -0,0 +1,2 @@
+public class MeinTestArray {
+}
