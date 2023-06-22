2e29deeba31b69d16964ba268f9bba884009864b
enji777 <enji777>
2023-06-22 15:23:35 +0200

    Arrays 22.6.2023

--- Intellij/Arrays/src/MeinArray.java
+++ Intellij/Arrays/src/MeinArray.java
@@ -9,14 +9,32 @@ public class MeinArray {
         }
     }
 
-    public static void printArray(float[] array){
-        for (int i= 0; i< array.length; i++){
-            System.out.println(array[i]);
+    public static void printArray(float[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
     }
 
-    public static void gibElement(int [] array, int i){
-        for (int i = 0; i < array.length; i++)
-            return
+    public static void printArray2(float[] array) {
+        for (float elem : array) {
+            System.out.print(elem + " ");
+        }
+    }
+
+    public static float gibElement(float[] array, int i) {
+        if (i < 0 || i >= array.length) {
+            return Float.MIN_VALUE;
+        }
+        return array[i];
+    }
+
+    public static float summiere(float[] array){
+        float summe = 0;
+        for (int i = 0; i < array.length; i++) {
+            summe = summe + array[i];
+        }
+        return summe;
+        }
+
     }
 }
--- Intellij/Arrays/src/MeinTestArray.java
+++ Intellij/Arrays/src/MeinTestArray.java
@@ -3,11 +3,18 @@ public class MeinTestArray {
         float[] array1 = {1.0f, 2.0f, 3.0f};
         float[] array2 = {4.0f, 5.0f};
         int[] array3 = {2,12,22};
-        int [] array4 ={33,300,3}
+        int [] array4 ={33,300,3};
 
         System.out.println(MeinArray.istGleichlang(array1,array2));
 
         MeinArray.printArray(array1);
+        MeinArray.printArray2(array1);
+        System.out.println();
+        System.out.println(MeinArray.gibElement(array1, 1));
+        System.out.println(MeinArray.gibElement(array1, 3));
+        System.out.println(MeinArray.gibElement(array1, -5));
+
+        System.out.println(MeinArray.summiere(array1));
 
     }
 
