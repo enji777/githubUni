b02887e41a207a0540fd7e6654227f2a74139da0
enji777 <hnyeen12@gmail.com>
2023-06-23 17:06:45 +0200

    Aufgabe 8 8.1-8.4

--- Intellij/Arrays/src/MeinArray.java
+++ Intellij/Arrays/src/MeinArray.java
@@ -28,13 +28,53 @@ public class MeinArray {
         return array[i];
     }
 
-    public static float summiere(float[] array){
+    public static float summiere(float[] array) {
         float summe = 0;
         for (int i = 0; i < array.length; i++) {
             summe = summe + array[i];
         }
         return summe;
+    }
+
+    public static float mittelwert(float[] array) {
+
+        return summiere(array) / array.length;
+    }
+
+    public static float max(float[] array) {
+        float max = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    public static boolean istGleich(char[] a1, char[] a2) {
+        for (int i = 0; i < a1.length; i++) {
+            if (a1[i] != a2[i])
+                return false;
+        }
+        return true;
+    }
+
+    public static String gibErstenIndexUngleicherElemente(char[] a1, char[] a2) {
+        for (int i = 0; i < a1.length; i++) {
+            if (a1[i] != a2[i]) {
+                return String.valueOf(i);
+            }
         }
 
+        return "kein ungleiches Element";
+    }
+
+    public static String gibErstesUngleichesElement(char[] a1, char[] a2){
+        for (int i = 0; i < a1.length; i++) {
+            if (a1[i] != a2[i]) {
+                return String.valueOf(a1[i]);
+            }
+        }
+        return "kein ungleiches Element";
     }
 }
--- Intellij/Arrays/src/MeinTestArray.java
+++ Intellij/Arrays/src/MeinTestArray.java
@@ -3,7 +3,11 @@ public class MeinTestArray {
         float[] array1 = {1.0f, 2.0f, 3.0f};
         float[] array2 = {4.0f, 5.0f};
         int[] array3 = {2,12,22};
-        int [] array4 ={33,300,3};
+        float [] array4 ={33,300,3};
+        float[] array5 = null;
+        char[] array6 = {'a', 'b', 'd'};
+        char[] array7 = {'a', 'b', 'c'};
+        char[] array8 =  {'a', 'b'};
 
         System.out.println(MeinArray.istGleichlang(array1,array2));
 
@@ -15,7 +19,11 @@ public class MeinTestArray {
         System.out.println(MeinArray.gibElement(array1, -5));
 
         System.out.println(MeinArray.summiere(array1));
-
+        System.out.println(MeinArray.mittelwert(array1));
+        System.out.println(MeinArray.max(array4));
+        System.out.println(MeinArray.gibErstenIndexUngleicherElemente(array6,array7));
+        System.out.println(MeinArray.gibErstesUngleichesElement(array6,array7));
+        System.out.println(MeinArray.gibErstesUngleichesElement(array6,array8));
     }
 
 }
